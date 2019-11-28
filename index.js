10e285fc1e0e4ac5c06c9fa93ce5bcee95ccc66f
Mayowa Ojo <mayowaojo.e@gmail.com>
2019-11-28 05:48:45 -0700

    initialize git repository

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('click', function() {
+    // alert('This is a chrome extension')
+    // chrome.storage.sync.set({message: "Chakra chrome extension is live"}, function() {
+    //     alert("working...")
+    // })
+
+    // chrome.storage.sync.get(['message'], function(res) {
+    //     alert(res.message)
+    // })
+    console.log('clicked')
+})
